ceb2ef7a474550fc401f0a42dbcecdcfccb012aa
mila <milamarcos98@gmail.com>
2024-11-23 02:14:47 -0300

    env

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { userRoutes } from './routes/index.js';
 import 'dotenv/config';
 import cors from "cors"
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanban", {
+mongoose.connect(process.env.CONNECT, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
